b9355c184b2155c4680f4cb1fe9a0966e70642d9
georgelimadev <george.lima@vtex.com.br>
2024-07-25 10:08:38 -0300

    Done: cadastrar empresa. WIP: página de login

--- src/main/java/br/edu/ifpb/pweb2/estagiotrack/controller/EmpresaController.java
+++ src/main/java/br/edu/ifpb/pweb2/estagiotrack/controller/EmpresaController.java
@@ -1,7 +1,7 @@
 package br.edu.ifpb.pweb2.estagiotrack.controller;
 
-import br.edu.ifpb.pweb2.estagiotrack.model.Aluno;
-import br.edu.ifpb.pweb2.estagiotrack.repository.AlunoRepository;
+import br.edu.ifpb.pweb2.estagiotrack.model.Empresa;
+import br.edu.ifpb.pweb2.estagiotrack.repository.EmpresaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,29 +11,29 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
-@RequestMapping("/alunos")
-public class AlunoController {
+@RequestMapping("/empresas")
+public class EmpresaController {
 
 
     @RequestMapping("/form")
-    public String getForm(Aluno aluno, Model model){
-        model.addAttribute("aluno", aluno);
-        return "alunos/form";
+    public String getForm(Empresa empresa, Model model){
+        model.addAttribute("empresa", empresa);
+        return "empresas/form";
     }
 
     @Autowired
-    private AlunoRepository alunoRepository;
+    private EmpresaRepository empresaRepository;
 
     //Falta ajustar o redirect
     @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String cadastroAluno(Aluno aluno, Model model, RedirectAttributes attr){
-        if(aluno.getNome().isEmpty() || aluno.getEmail().isEmpty() || aluno.getSenha().isEmpty()){
+    public String cadastroEmpresa(Empresa empresa, Model model, RedirectAttributes attr){
+        if(empresa.getRazaoSocial().isEmpty() || empresa.getEmail().isEmpty() || empresa.getSenha().isEmpty()){
             model.addAttribute("alert", "Por favor preencha todos os campos corretamente.");
-            return "alunos/form";
+            return "empresas/form";
         } else {
-            alunoRepository.save(aluno);
-            model.addAttribute("alunos",alunoRepository.findAll());
-            return "alunos/list";
+            empresaRepository.save(empresa);
+            model.addAttribute("empresas",empresaRepository.findAll());
+            return "empresas/list";
             //attr.addFlashAttribute("mensagem", "Aluno cadastrado com sucesso!");
             //return "redirect:/alunos/list";
         }
---
+++ src/main/java/br/edu/ifpb/pweb2/estagiotrack/repository/EmpresaRepository.java
@@ -0,0 +1,40 @@
+package br.edu.ifpb.pweb2.estagiotrack.repository;
+
+import org.springframework.stereotype.Component;
+
+import br.edu.ifpb.pweb2.estagiotrack.model.Empresa;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+@Component
+public class EmpresaRepository {
+    private Map<Integer,Empresa> repositorio = new HashMap<Integer, Empresa>();
+
+    public Empresa findById(Integer id){ return repositorio.get(id);}
+
+    public void save(Empresa empresa){
+        Integer id = null;
+        id = (empresa.getId() == null) ? this.getMaxId() + 1: empresa.getId();
+        empresa.setId(id);
+        repositorio.put(id,empresa);
+    }
+
+    public List<Empresa> findAll(){
+        List<Empresa> empresas = repositorio.values().stream().collect(Collectors.toList());
+        return empresas;
+    }
+
+    public Integer getMaxId(){
+        List<Empresa> empresas = findAll();
+        if(empresas == null || empresas.isEmpty())
+          return 1;
+
+       Empresa contaMaxId = empresas
+               .stream()
+               .max(Comparator.comparing(Empresa::getId))
+               .orElseThrow(NoSuchElementException::new);
+       return contaMaxId.getId() == null ? 1 : contaMaxId.getId() + 1;
+    }
+}
