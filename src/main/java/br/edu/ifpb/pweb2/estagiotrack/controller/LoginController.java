ba56b0d08d5eae1d663dd47eb71f39ddd904e3ce
georgelimadev <george.lima@vtex.com.br>
2024-07-26 08:38:18 -0300

    Ajustando mock de login no front

--- src/main/java/br/edu/ifpb/pweb2/estagiotrack/controller/LoginController.java
+++ src/main/java/br/edu/ifpb/pweb2/estagiotrack/controller/LoginController.java
@@ -1,12 +1,8 @@
 package br.edu.ifpb.pweb2.estagiotrack.controller;
 
-import br.edu.ifpb.pweb2.estagiotrack.model.Aluno;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/")
@@ -17,5 +13,3 @@ public class LoginController {
     }
 
 }
-
-
