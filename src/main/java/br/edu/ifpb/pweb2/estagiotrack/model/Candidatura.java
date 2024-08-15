d2857ba3dcab321eaf63331d26a2f04de80db746
georgelimadev <george.lima@vtex.com.br>
2024-08-15 11:35:28 -0300

    WIP: Adicionando classes Candidatura e Estagio ao modelo

---
+++ src/main/java/br/edu/ifpb/pweb2/estagiotrack/model/Candidatura.java
@@ -0,0 +1,8 @@
+package br.edu.ifpb.pweb2.estagiotrack.model;
+
+public class Candidatura {
+    //Deve conter os seguintes atributos:
+    // Aluno alunoCandidato;
+    // Oferta ofertaSelecionada;
+    // O listAll dessa classe será usado para o requisito de coordenador ver candidaturas.
+}
---
+++ src/main/java/br/edu/ifpb/pweb2/estagiotrack/model/Estagio.java
@@ -0,0 +1,8 @@
+package br.edu.ifpb.pweb2.estagiotrack.model;
+
+public class Estagio {
+    //Deve conter os seguintes atributos:
+    // Aluno alunoCandidato;
+    // Oferta ofertaSelecionada;
+    // A Oferta deve ser transformada em Estágio.
+}
