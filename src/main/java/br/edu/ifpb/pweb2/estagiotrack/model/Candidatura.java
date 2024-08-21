e499263bb0cc8d469dcff3ba76262f551f1d65d8
Brianrafs <brianxdgmr@gmail.com>
2024-08-20 22:47:14 -0300

    feat(models): Implementado o jakarta persistence e validators aos modelos

--- src/main/java/br/edu/ifpb/pweb2/estagiotrack/model/Candidatura.java
+++ src/main/java/br/edu/ifpb/pweb2/estagiotrack/model/Candidatura.java
@@ -1,26 +1,33 @@
 package br.edu.ifpb.pweb2.estagiotrack.model;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import br.edu.ifpb.pweb2.estagiotrack.model.enums.StatusCandidatura;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
 
 import java.io.Serializable;
 
 @Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Candidatura implements Serializable {
 
     @Setter
     @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Integer id;
-    public Integer idOferta;
+
+    @Email
     public String emailCandidato;
+
+    @ManyToOne
     public Aluno alunoCandidato;
+
+    @ManyToOne
     public Oferta ofertaSelecionada;
 
-    public Candidatura(Integer idOferta, String emailCandidato, Aluno alunoCandidato, Oferta ofertaSelecionada) {
-        this.idOferta = idOferta;
-        this.emailCandidato = emailCandidato;
-        this.alunoCandidato = alunoCandidato;
-        this.ofertaSelecionada = ofertaSelecionada;
-    }
+    @Enumerated(EnumType.STRING)
+    private StatusCandidatura statusCandidatura;
 }
--- src/main/java/br/edu/ifpb/pweb2/estagiotrack/model/Oferta.java
+++ src/main/java/br/edu/ifpb/pweb2/estagiotrack/model/Oferta.java
@@ -1,30 +1,48 @@
 package br.edu.ifpb.pweb2.estagiotrack.model;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import br.edu.ifpb.pweb2.estagiotrack.model.enums.StatusOferta;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
 import java.io.Serializable;
 
 @Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Oferta implements Serializable {
 
     @Setter
     @Getter
+    @Id
+    @GeneratedValue
     public Integer id;
+
+    @ManyToOne
     public Empresa ofertante;
-    public String emailOfertante; // Parte do workaround para vincular oferta a empresa dinamicamente.
+
+    @Email
+    public String emailOfertante;
+
+    @NotBlank(message = "O Nome do cargo não pode ser vazio")
     public String tituloCargo;
+
+    @NotBlank(message = "O valor da bolsa não pode ser vazio")
     public String valorBolsa;
+
+    @NotBlank
     public String turno;
 
-    public Oferta(Integer id, Empresa ofertante, String emailOfertante, String tituloCargo, String valorBolsa,
-            String turno) {
-        this.id = id;
-        this.ofertante = ofertante;
-        this.emailOfertante = emailOfertante;
-        this.tituloCargo = tituloCargo;
-        this.valorBolsa = valorBolsa;
-        this.turno = turno;
+    @Enumerated(EnumType.STRING)
+    private StatusOferta statusOferta;
+
+    public void encerrar() {
+        this.statusOferta = StatusOferta.ENCERRADA;
+    }
+
+    public StatusOferta getStatus() {
+        return this.statusOferta;
     }
 }
