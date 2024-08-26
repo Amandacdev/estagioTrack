29d50ff9b48ada7b4e75597e72d500bfb43802e7
georgelimadev <george.lima@vtex.com.br>
2024-08-26 16:24:09 -0300

    Bugfix: listando candidaturas do usuário informado

--- src/main/java/br/edu/ifpb/pweb2/estagiotrack/controller/paginaUsuarioController.java
+++ src/main/java/br/edu/ifpb/pweb2/estagiotrack/controller/paginaUsuarioController.java
@@ -1,4 +1,5 @@
 package br.edu.ifpb.pweb2.estagiotrack.controller;
+
 import br.edu.ifpb.pweb2.estagiotrack.model.*;
 import br.edu.ifpb.pweb2.estagiotrack.service.AlunoService;
 import br.edu.ifpb.pweb2.estagiotrack.service.CandidaturaService;
@@ -9,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import br.edu.ifpb.pweb2.estagiotrack.model.Candidatura;
 import br.edu.ifpb.pweb2.estagiotrack.repository.CandidaturaRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,7 +29,6 @@ public class paginaUsuarioController {
         return "consultaEstudanteEmpresa";
     }
 
-
     // variáveis que serão usadas na verificação se existe o email buscado no filtro
     @Autowired
     private AlunoService alunoService;
@@ -42,17 +41,21 @@ public class paginaUsuarioController {
     private final CandidaturaRepository candidaturaRepository;
 
     @Autowired
-    public paginaUsuarioController(AlunoService alunoService, EmpresaService empresaService, CandidaturaRepository candidaturaRepository) {
+    public paginaUsuarioController(AlunoService alunoService, EmpresaService empresaService,
+            CandidaturaRepository candidaturaRepository) {
         this.alunoService = alunoService;
         this.empresaService = empresaService;
         this.candidaturaRepository = candidaturaRepository;
     }
 
-
-    /*Recebe um email e verifica:
-        Se o email é de um estudante, direciona para a página que exibe as candidatutas do estudante solicitante
-        Se o email é de uma empresa, direciona para a página que exibe as ofertas da empresa solicitante, com buttons de ação
-        Se não identificado, mensagem de erro*/
+    /*
+     * Recebe um email e verifica:
+     * Se o email é de um estudante, direciona para a página que exibe as
+     * candidatutas do estudante solicitante
+     * Se o email é de uma empresa, direciona para a página que exibe as ofertas da
+     * empresa solicitante, com buttons de ação
+     * Se não identificado, mensagem de erro
+     */
 
     @PostMapping("/identificar")
     public String pesquisarSolicitante(@RequestParam("emailFiltro") String emailFiltro, Model model) {
@@ -63,44 +66,48 @@ public class paginaUsuarioController {
         // Checando se tem empresa com esse email
         Empresa empresa = empresaService.findByEmail(emailFiltro).orElse(null);
 
-        if (aluno != null && empresa == null){
-            candidaturaController.getListCandidaturasUsuario(model,aluno);
-            //return "redirect:/paginaUsuario/estudantePage?email=" + emailFiltro;
-        }if (aluno == null && empresa != null){
-            return "redirect:/paginaUsuario/empresaPage";
+        if (aluno != null && empresa == null) {
+            candidaturaController.getListCandidaturasUsuario(model, aluno);
+            return "paginaUsuario/candidaturasEstudante";
         }
-        else {
+        if (aluno == null && empresa != null) {
+            return "redirect:/paginaUsuario/empresaPage";
+        } else {
             model.addAttribute("erro", "Não foi encontrado um cadastro com esse email. Tente novamente.");
-            return "redirect:/formParaFiltroPesquisa";
-            //ajustar para funcionar de forma a exibir a mensagem de erro: return "formParaFiltroPesquisa";
+            return "redirect:/paginaUsuario/formParaFiltroPesquisa";
+            // ajustar para funcionar de forma a exibir a mensagem de erro: return
+            // "formParaFiltroPesquisa";
         }
     }
     /*
-    @RequestMapping("estudantePage")
-    public String getEstudantePage(@RequestParam("emailFiltro") String emailFiltro, Model model) {
+     * @RequestMapping("estudantePage")
+     * public String getEstudantePage(@RequestParam("emailFiltro") String
+     * emailFiltro, Model model) {
+     * 
+     * // Buscando o estudante pelo email
+     * Aluno aluno = alunoService.findByEmail(emailFiltro).orElse(null);
+     * 
+     * // Verificando se o aluno foi encontrado
+     * if (aluno == null) {
+     * model.addAttribute("erro", "Estudante não encontrado.");
+     * return "paginaUsuario/candidaturasEstudante"; // Ou redirecionar para uma
+     * página de erro
+     * }
+     * 
+     * // Buscando as candidaturas do estudante
+     * List<Candidatura> candidaturasDoUsuario =
+     * candidaturaRepository.findByEmail(emailFiltro);
+     * 
+     * // Adicionando as candidaturas e os dados do aluno ao modelo
+     * model.addAttribute("aluno", aluno);
+     * model.addAttribute("candidaturas", candidaturasDoUsuario);
+     * 
+     * return "paginaUsuario/candidaturasEstudante";
+     * }
+     * 
+     */
 
-        // Buscando o estudante pelo email
-        Aluno aluno = alunoService.findByEmail(emailFiltro).orElse(null);
-
-        // Verificando se o aluno foi encontrado
-        if (aluno == null) {
-            model.addAttribute("erro", "Estudante não encontrado.");
-            return "paginaUsuario/candidaturasEstudante"; // Ou redirecionar para uma página de erro
-        }
-
-        // Buscando as candidaturas do estudante
-        List<Candidatura> candidaturasDoUsuario = candidaturaRepository.findByEmail(emailFiltro);
-
-        // Adicionando as candidaturas e os dados do aluno ao modelo
-        model.addAttribute("aluno", aluno);
-        model.addAttribute("candidaturas", candidaturasDoUsuario);
-
-        return "paginaUsuario/candidaturasEstudante";
-    }
-
-    */
-
-        @RequestMapping("empresaPage")
+    @RequestMapping("empresaPage")
     public String getEmpresaPage() {
         return "paginaUsuario/ofertasEmpresa";
     }
