dcfbaffd937ae7987beeaf171f5d88511b5f1df8
georgelimadev <george.lima@vtex.com.br>
2024-08-27 15:37:17 -0300

    Testes ok. Merge liberado

--- src/main/java/br/edu/ifpb/pweb2/estagiotrack/controller/LoginController.java
+++ src/main/java/br/edu/ifpb/pweb2/estagiotrack/controller/LoginController.java
@@ -1,15 +1,22 @@
 package br.edu.ifpb.pweb2.estagiotrack.controller;
+
 import br.edu.ifpb.pweb2.estagiotrack.model.Aluno;
 import br.edu.ifpb.pweb2.estagiotrack.model.Empresa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import br.edu.ifpb.pweb2.estagiotrack.service.AlunoService;
-import br.edu.ifpb.pweb2.estagiotrack.service.EmpresaService;
+import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/")
 public class LoginController {
+
+    @Autowired
+    private AlunoController alunoController;
+
+    @Autowired
+    private EmpresaController empresaController;
+
     @GetMapping("/login")
     public String showLoginPage() {
         return "login";
@@ -21,33 +28,23 @@ public class LoginController {
         return "consultaEstudanteEmpresa";
     }
 
-    /*
-     * Recebe um email e verifica:
-     * Se o email é de um estudante, direciona para a página que exibe as
-     * candidatutas do estudante solicitante
-     * Se o email é de uma empresa, direciona para a página que exibe as ofertas da
-     * empresa solicitante, com buttons de ação
-     * Se não identificado, mensagem de erro
-     */
     @PostMapping("/identificar")
     public String pesquisarSolicitante(@RequestParam("emailFiltro") String emailFiltro, Model model) {
 
         // Checando se tem estudante com esse email
-        Aluno aluno = alunoService.findByEmail(emailFiltro).orElse(null);
+        Aluno aluno = alunoController.buscarPorEmail(emailFiltro);
 
         // Checando se tem empresa com esse email
-        Empresa empresa = empresaService.findByEmail(emailFiltro).orElse(null);
+        Empresa empresa = empresaController.buscarPorEmail(emailFiltro);
 
-        if (aluno != null && empresa == null) {
+        if (aluno != null) {
             return "redirect:/paginaUsuario/estudantePage";
         }
-        if (aluno == null && empresa != null) {
+        if (empresa != null) {
             return "redirect:/paginaUsuario/empresaPage";
         } else {
             model.addAttribute("erro", "Não foi encontrado um cadastro com esse email. Tente novamente.");
-            return "redirect:/formParaFiltroPesquisa";
-            // ajustar para funcionar de forma a exibir a mensagem de erro: return
-            // "formParaFiltroPesquisa";
+            return "consultaEstudanteEmpresa";
         }
     }
 }
