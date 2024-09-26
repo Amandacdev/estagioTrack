1478150c149ff81b20691571374d6aa95ddd3b1d
georgelimadev <george.lima@vtex.com.br>
2024-09-26 09:58:43 -0300

    BUGFIX: fluxo de cadastro de estágio

--- src/main/java/br/edu/ifpb/pweb2/estagiotrack/model/Estagio.java
+++ src/main/java/br/edu/ifpb/pweb2/estagiotrack/model/Estagio.java
@@ -1,16 +1,18 @@
 package br.edu.ifpb.pweb2.estagiotrack.model;
 
-import br.edu.ifpb.pweb2.estagiotrack.model.enums.StatusOferta;
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import org.springframework.format.annotation.DateTimeFormat;
-import java.time.LocalDate;
 
 @Data
 @Entity
@@ -23,18 +25,23 @@ public class Estagio {
     private Integer id;
 
     @OneToOne
+    @NotBlank(message = "O campo Estudante é obrigatório.")
     private Aluno alunoAprovado;
 
     @OneToOne
+    @NotBlank(message = "O campo Oferta é obrigatório.")
     private Oferta ofertaSelecionada;
 
-    @OneToOne
+    @ManyToOne
+    @NotBlank(message = "O campo Candidatura é obrigatório.")
     private Candidatura candidaturaSelecionada;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @NotBlank(message = "O campo Data de início é obrigatório.")
     private LocalDate dataInicio;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @NotBlank(message = "O campo Data de término é obrigatório.")
     private LocalDate dataFim;
 
     private String observacoes;
